5a28bf513759b271ae7e4f32a537c668e1a6def9
Svatoslav <svetsvetonosov@mail.ru>
2023-03-12 18:47:26 +0300

    Add homework

--- Documents/GeekBrains/C#_1/Seminar2/Program.cs
+++ Documents/GeekBrains/C#_1/Seminar2/Program.cs
@@ -1,17 +1,50 @@
-﻿// Напишите программу, которая выводит случайное число из отрезка 
-// [10, 99] и показывает наибольшую цифру числа.
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
 
-int num =new Random().Next(10,100);
-Console.WriteLine(num);
 
-int a= num/10;
-int b = num%10;
+// Console.WriteLine("Введите трехзначное число");
+// int number1=Convert.ToInt32(Console.ReadLine());
 
-if(a>b)
+// int number2 = (number1%100 - number1%10)/10 ;
+// {
+//     Console.WriteLine("Второе число: " + number2);
+// }
+
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+// Console.WriteLine("Введите число");
+// int number1=Convert.ToInt32(Console.ReadLine());
+
+// int number2 = (number1%1000 - number1%100)/100 ;
+
+// if (number1 < 100)
+// {
+//     Console.WriteLine("Третьего числа нет");
+// }
+// else if (number1 > 100)
+// {
+//     Console.WriteLine("Третье число: " + number2);
+// }
+
+
+
+// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите цифру дня недели");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 <= 5)
 {
-    Console.WriteLine("Наибольшая цифра: "+ a);
+    Console.WriteLine("Не выходной день");
 }
-else
+else if (number1 == 6 || number1 == 7)
 {
-    Console.WriteLine("Наибольшая цифра: "+ b);
+    Console.WriteLine("Выходной день");
+}
+else 
+{
+    Console.WriteLine("Такого дня недели не существует");
 }
